f0bf0fd3f1ccc032804d777f8d87262d22bc6b81
liudmila.tsekat <liudmila.tsekat@gmail.com>
2020-02-24 12:18:11 +0100

    5 task

---
+++ src/task_5/Solution_01.java
@@ -0,0 +1,10 @@
+package task_5;
+
+/***
+ * Написать программу, выводящую на экран надпись "B5 volkswagen passat - бесценна" 5 раз. Каждый раз - с новой строки.
+ */
+public class Solution_01 {
+    public static void main(String[] args) {
+
+    }
+}
