2109cff18238edb3ff7f815caf15aa936086a3e1
sergey-tsekot <tsekot.sergey@gmail.com>
2020-03-05 11:17:01 +0100

    comments to solved tasks

--- src/task_5/Solution_02.java
+++ src/task_5/Solution_02.java
@@ -1,7 +1,8 @@
 package task_5;
+
 /**
  * Произведение 10 чисел
- *
+ * <p>
  * Вывести на экран произведение 10 чисел от 1 до 10
  * Результат это 1 число.
  * Подсказка:
@@ -9,11 +10,26 @@ package task_5;
  */
 public class Solution_02 {
     public static void main(String[] args) {
+        implementationUsingIf();
+        implementationWithoutIf();
+    }
+
+    private static void implementationWithoutIf() {
         int sum = 1;
         for (int i = 1; i <= 10; i++) {
-            sum=sum*i;
+            sum = sum * i;
+        }
+
+        System.out.println("Result is: " + sum);
+    }
+
+    private static void implementationUsingIf() {
+        int sum = 1;
+        for (int i = 1; i <= 10; i++) {
+            sum = sum * i;
             if (sum > 3000000)
-            System.out.println(sum);
-                }
+                System.out.println(sum);
+        }
     }
 }
+
--- src/task_5/Solution_03.java
+++ src/task_5/Solution_03.java
@@ -1,24 +1,34 @@
 package task_5;
+
 /**
- Таблица умножения
- Выведи на экран таблицу умножения 10 на 10 в следующем виде:
- 1 2 3 …
- 2 4 6 …
- 3 6 9 …
- …
+ * Таблица умножения
+ * Выведи на экран таблицу умножения 10 на 10 в следующем виде:
+ * 1 2 3 …
+ * 2 4 6 …
+ * 3 6 9 …
+ * …
  */
 public class Solution_03 {
     public static void main(String[] args) {
+        usingWhileLoop();
+        usingForLoop();
+    }
+
+    private static void usingForLoop() {
+        //try to solve in good mood
+    }
+
+    private static void usingWhileLoop() {
         int a = 1;
         int b = 1;
-        while (a<=10){
-            while (b<=10){
-                System.out.print( a * b + "" );
+        while (a <= 10) {
+            while (b <= 10) {
+                System.out.print(a * b + " ");
                 b++;
             }
             System.out.println("");
             a++;
-            b=1;
+            b = 1;
         }
     }
 }
