a5783fba34598c13cb7440c17eb217c8378b2e74
TsekotAndrey <61335008+TsekotAndrey@users.noreply.github.com>
2020-03-11 21:58:20 +0300

    7 task is completed

--- src/task_7/Calculator.java
+++ src/task_7/Calculator.java
@@ -5,42 +5,67 @@ public class Calculator {
     public int getMinFromTreeValues(int first, int second, int third) {
         if (first < second) {
             if (first < third) {
-                return third;
-            } else {
                 return first;
+            } else {
+                return second;
             }
         } else {
             if (second < third) {
                 return second;
             } else {
-                return second;
+                return third;
             }
         }
     }
 
     public int getMinFromFourValues(int first, int second, int third, int fourth) {
+        if (first < second) {
+            if (first < third)
+                if (first < fourth) {
+                    return first;
+                } else {
+                    return second;
+                }
+        } else {
+            if (second < third) {
+                if (second < fourth)
+                return second;
+            } else {
+                return third;
+            }
+            if (third < fourth) {
+                return third;
+            } else {
+                return fourth;
+            }
+        }
         return 0;
     }
 
+
     public int getMax(int first, int second) {
-        return Math.min(first, second);
+        return Math.max(first, second);
     }
 
     public int getMaxFromThree(int first, int second, int third) {
         int firstMax = Math.max(first, second);
         int secondMax = Math.max(second, third);
-        return firstMax > secondMax ? secondMax : firstMax;
+        return Math.max(firstMax, secondMax);
     }
 
     public int calculate(int firstValue, int secondValue, String operation) {
         int res = 0;
         switch (operation) {
+
             case "+":
                 res = firstValue + secondValue;
-
-            case "-":
-                res = firstValue - secondValue;
-
+                break;
+            case "*":
+                res = firstValue * secondValue;
+                break;
+            case "/":
+                res = firstValue / secondValue;
+                break;
         }
         return res;
     }
